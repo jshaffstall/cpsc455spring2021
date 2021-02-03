025530a577d4c695ae58c04817481976ee0f5929
Kenyarra Williams <kwebster@muskingum.edu>
2021-02-03 12:29:51 -0500

    changes to log in, getting error could not find driver

--- webapp/www/login.php
+++ webapp/www/login.php
@@ -2,31 +2,13 @@
 
 require 'config.php';
 
-$server = 'localhost';
-$user = 'cpsc455user';
-$pass = 'cpsc455spring2021';
-$db = 'cpsc455spring2021';
-
-try{
-	
-	$conn = new PDO("mysql:host=$server;dbname=$db", $user, $pass);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-} catch (PDOException $e) {
-		
-		print("Error: " + $e->getMessage());
-		
-	
-}
-
-
 
 if(isset($_POST['email'])){
 	$email = $_POST['email'];
 	$query = $conn->prepare("select password from user where email like '$email'");
 	
 	$password = $_POST['password'];
-	if(password_verify($password, $query)){
+	if(login_user($email, $password) != False){
 		session_regenerate_id(true);
 		header("Location: http://localhost/cpsc455spring2021/webapp/www/index.php");
 		exit();
