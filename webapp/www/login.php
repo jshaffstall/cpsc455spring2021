65144cf419fb8d439caa6f04dfd66e8b703cfd8e
Kenyarra Williams <kwebster@muskingum.edu>
2021-02-03 16:23:38 -0500

    login page checks and redirects user to homepage

--- webapp/www/login.php
+++ webapp/www/login.php
@@ -5,16 +5,19 @@ require 'config.php';
 
 if(isset($_POST['email'])){
 	$email = $_POST['email'];
-	$query = $conn->prepare("select password from user where email like '$email'");
-	
 	$password = $_POST['password'];
+	
 	if(login_user($email, $password) != False){
-		session_regenerate_id(true);
+		$id = session_regenerate_id(true);
+		$_SESSION['user'] = $email;
 		header("Location: http://localhost/cpsc455spring2021/webapp/www/index.php");
 		exit();
 	}
+	else{
+		print("Email/Password combination not found");
+	}
 }
 
-echo $twig->render('login.html',[]);
+echo $twig->render('login.html',['user' => $user]);
 
 ?>
