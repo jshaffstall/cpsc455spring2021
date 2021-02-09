13073f8519901d1804db1c8a1d99338cd19c675c
Kenyarra Williams <kwebster@muskingum.edu>
2021-02-09 17:46:45 -0500

    added header to display-form to check if correct reroute

--- webapp/www/display-form.php
+++ webapp/www/display-form.php
@@ -2,7 +2,10 @@
 
 require 'config.php';
 	
+$forms = get_forms();
+$fields = get_form_fields(1);
+$types = get_form_field_types();
 
-	echo $twig->render('display-form.html',[]);
+echo $twig->render('display-form.html',['forms' => $forms, 'fields' => $fields, 'types' => $types]);
 
 ?>
