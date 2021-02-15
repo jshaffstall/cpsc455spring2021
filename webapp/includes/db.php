535b010cf65471ffc4a6ac85e4564d3b19eeee89
Jay Shaffstall <jshaffstall@gmail.com>
2021-02-15 12:49:43 -0500

    Another SQL fix

--- webapp/includes/db.php
+++ webapp/includes/db.php
@@ -216,7 +216,7 @@ function get_form_field($formfield)
 {
     global $pdo;
 
-    $sql = "SELECT * FROM formfields WHERE formfields.id=:formfield";
+    $sql = "SELECT formfields.id, formfields.form, formfields.label, formfields.type, formfields.default, formfields.order, formfieldtypes.name FROM formfields, formfieldtypes WHERE formfields.id=:formfield and formfields.type=formfieldtypes.id";
     
 	//$sql = "SELECT * FROM formfields WHERE form=:form ORDER BY `order`";
 	
