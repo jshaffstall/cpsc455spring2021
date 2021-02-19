3fe0132cdcd8265bc9d277929ce705bca4174ea7
Jay <jshaffstall@gmail.com>
2021-02-19 12:01:26 -0500

    Removed test code from db_example.php

--- webapp/www/db_example.php
+++ webapp/www/db_example.php
@@ -10,8 +10,4 @@ $forms = get_forms();
 $fields = get_form_fields(1);
 $field = get_form_field(1);
 
-echo add_form_field (1, 1, 'label', '', 1, 'foo');
-echo add_form_field (1, 1, 'label', '', 1, 'foo');
-update_form_field (1, 2, 1, 'label 2', '', 1, 'foo2');
-
 echo $twig->render('db_example.html', ['roles' => $roles, 'types' => $types, 'forms' => $forms, 'fields' => $fields]);
