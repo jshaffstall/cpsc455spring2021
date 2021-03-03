74381e063e6010ea6b206ee517322cef0e220aca
Kenyarra Williams <kwebster@muskingum.edu>
2021-03-03 13:27:12 -0500

    submit updated with db changes

--- webapp/www/display-form.php
+++ webapp/www/display-form.php
@@ -4,10 +4,19 @@ require 'config.php';
 
 //var_dump($_GET);
 
-$_GET['form'];
-$types = get_form_field_types();
-$fields = get_form_fields($_GET['form']);
 
-echo $twig->render('display-form.html',['fields' => $fields, 'types' => $types]);
+
+if(isset($_POST['formid'])){
+	submit_form($user['id'], $_POST['formid'], $_POST);
+}
+else{
+	$_GET['form'];
+	$types = get_form_field_types();
+	$fields = get_form_fields($_GET['form']);
+	
+	echo $twig->render('display-form.html',['fields' => $fields, 'types' => $types, 'form' => $_GET['form']]);
+}
+
+
 
 ?>
