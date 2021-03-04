34b2442d4c3b24077ba27794ff6b8034aa9b4c5a
Jordan <37128940+DeekStokes@users.noreply.github.com>
2021-03-04 15:42:19 -0500

    Added enabling and disabled users

--- webapp/www/users.php
+++ webapp/www/users.php
@@ -6,3 +6,21 @@ $enabledUsers = get_users();
 $disabledUsers = get_users(1);
 
 echo $twig->render('users.html', ['enabledUsers' => $enabledUsers, 'disabledUsers' => $disabledUsers]);
+
+if (isset($_POST['submitDisableUsers'])) {
+	$usersToDisable = $_POST['usersToDisable'];
+	
+	foreach ($usersToDisable as $userId) {
+		disable_user($userId);
+		header("Location: users.php");
+	}
+}
+
+else if (isset($_POST['submitEnableUsers'])) {
+	$usersToEnable = $_POST['usersToEnable'];
+	
+	foreach ($usersToEnable as $userId) {
+		enable_user($userId);
+		header("Location: users.php");
+	}
+}
