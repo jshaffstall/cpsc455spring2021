b6bbf6a94ec50560df4b79d1fa874a4ab64d4d0f
Jay <jshaffstall@gmail.com>
2021-03-10 12:03:50 -0500

    Fixed syntax error in display form php

--- webapp/www/display-form.php
+++ webapp/www/display-form.php
@@ -4,22 +4,18 @@ require 'config.php';
 
 //var_dump($_GET);
 
+if(isset($_POST['formid'])){
+	$submit = submit_form($user['id'], $_POST['formid'], $_POST);
+	//header("Location: list-forms.php");
+}
+
 $submitted = get_form_submission($user['id'], 4, null);
 
-if(! $submitted){
-	if(isset($_POST['formid'])){
-		$submit = submit_form($user['id'], $_POST['formid'], $_POST);
-		//header("Location: list-forms.php");
-	}
-	else{
-		$_GET['form'];
-		$types = get_form_field_types();
-		$fields = get_form_fields($_GET['form']);
-	
-		echo $twig->render('display-form.html',['fields' => $fields, 'types' => $types, 'form' => $_GET['form']]);
-	}
-}
-else{
+$submissions = array();
+
+if ($submitted)
+{
+	/*
 	$submissions = get_field_submissions(1);
 	$temp = array();
 	for ($submissions){
@@ -27,8 +23,13 @@ else{
 	}
 	var_dump($temp);
 	exit();
-	
+	*/
 }
+
+$types = get_form_field_types();
+$fields = get_form_fields($_GET['form']);
+
+echo $twig->render('display-form.html',['fields' => $fields, 'types' => $types, 'form' => $_GET['form'], 'submissions' => $submissions]);
 
 
 ?>
