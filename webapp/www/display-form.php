c36526f45782ec909c32ed3ddbb7092df4de65d3
Kenyarra Williams <kwebster@muskingum.edu>
2021-03-10 11:08:33 -0500

    display-form updates to implement old submissions

--- webapp/www/display-form.php
+++ webapp/www/display-form.php
@@ -4,19 +4,31 @@ require 'config.php';
 
 //var_dump($_GET);
 
+$submitted = get_form_submission($user['id'], 4, null);
 
-if(isset($_POST['formid'])){
-	$submit = submit_form($user['id'], $_POST['formid'], $_POST);
-	//header("Location: list-forms.php");
+if(! $submitted){
+	if(isset($_POST['formid'])){
+		$submit = submit_form($user['id'], $_POST['formid'], $_POST);
+		//header("Location: list-forms.php");
+	}
+	else{
+		$_GET['form'];
+		$types = get_form_field_types();
+		$fields = get_form_fields($_GET['form']);
+	
+		echo $twig->render('display-form.html',['fields' => $fields, 'types' => $types, 'form' => $_GET['form']]);
+	}
 }
 else{
-	$_GET['form'];
-	$types = get_form_field_types();
-	$fields = get_form_fields($_GET['form']);
+	$submissions = get_field_submissions(1);
+	$temp = array();
+	for ($submissions){
+		$temp[$submissions['name']] = $submissions;
+	}
+	var_dump($temp);
+	exit();
 	
-	echo $twig->render('display-form.html',['fields' => $fields, 'types' => $types, 'form' => $_GET['form']]);
 }
-
 
 
 ?>
