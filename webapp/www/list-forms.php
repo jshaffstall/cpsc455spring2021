15405c96f1c24880b78e557aa2615203d3d539ff
Kenyarra Williams <kwebster@muskingum.edu>
2021-03-15 12:00:06 -0400

    student and site form listings

--- webapp/www/list-forms.php
+++ webapp/www/list-forms.php
@@ -3,7 +3,9 @@
 require 'config.php';
 
 $forms = get_forms();
+$studentForms = get_student_forms();
+$siteForms = get_site_forms();
 
-echo $twig->render('list-forms.html',['forms' => $forms]);
+echo $twig->render('list-forms.html',['forms' => $forms, 'studentForms' => $studentForms, 'siteForms' => $siteForms]);
 
 ?>
