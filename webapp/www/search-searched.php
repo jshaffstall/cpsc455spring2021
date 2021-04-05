3a57b1195e1da88f8d7b82046e1b771752c31a28
Kenyarra Williams <kwebster@muskingum.edu>
2021-04-05 12:26:10 -0400

    form search

--- webapp/www/search-searched.php
+++ webapp/www/search-searched.php
@@ -2,6 +2,14 @@
 
 require 'config.php';
 
+//searchterms should an array with pairs of key and search words
+//create array with POST fieldname and POST search
 
-	echo $twig->render('search-searched.html',[]);
+$searchterms = array(
+	array($_POST['fieldname'], $_POST['searchterm'])
+);
+
+$search = search_form_submissions($_POST['formid'], $searchterms);
+
+	echo $twig->render('search-searched.html',['formid' => $_POST['formid'],'search' => $search]);
 ?>
