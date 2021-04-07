14800db08605b4bf8daed55bfb090aeb65cb3868
Jordan <37128940+DeekStokes@users.noreply.github.com>
2021-04-07 12:40:35 -0400

    Form creator and editor bug fixes

--- webapp/www/form-editor.php
+++ webapp/www/form-editor.php
@@ -8,11 +8,11 @@ require 'config.php';
 $formId = $_GET["form"];
 $form = get_form_by_id($formId);
 $types = get_form_field_types();
-
+$sites = get_sites();
 $roles = get_roles();
 $currentRole = null;
 
-if (isset($_POST['submitForm'])) {
+if (isset($_POST['editForm'])) {
 	editForm();
 } 
 else if (isset($_POST['submitField'])) {
@@ -28,7 +28,7 @@ if ($form) {
 	$formFields = get_form_fields($form["id"]);
 	$currentRole = $form["roleid"];
 	
-	$webpage = $twig->render('form-editor.html',['form' => $form, 'types' => $types, 'fields' => $formFields, 'roles' => $roles]);
+	$webpage = $twig->render('form-editor.html',['form' => $form, 'types' => $types, 'fields' => $formFields, 'roles' => $roles, 'sites' => $sites]);
 	echo "$webpage";
 }
 else {
@@ -40,19 +40,25 @@ function editForm() {
 	global $form;
 	
 	$formName = $_POST["formName"];
-	$role = $_POST["formRole"];
+	$roleId = $_POST["formRole"];
 	$forStudent = $_POST["forStudent"];
 	$siteVisible = $_POST["siteVisible"];
-	$siteId = $_POST["siteId"];
+	
+	if ($roleId == 3 && $forStudent) {
+		$siteId = null;
+	}
+	else {
+		$siteId = $_POST["siteId"];
+	}
 	
 	// return false if form of same name
 	// $test = update_form($form["id"], $formName, $role, $forStudent);
 	
-	$test = update_form($form["id"], $formName, $role, $forStudent, $siteVisible, $siteId);
+	$test = update_form($form["id"], $formName, $roleId, $forStudent, $siteVisible, $siteId);
 	var_dump($test);
 	$id = $form["id"];
 	
-	//header("Location: form-editor.php?form=$id");
+	header("Location: form-editor.php?form=$id");
 }
 
 function addFormField() {
