9fdcdc54bf60d19741f6e6638c0f9c771fd823bc
Kenyarra Williams <kwebster@muskingum.edu>
2021-04-11 22:21:07 -0400

    Search function working

--- webapp/www/search-searched.php
+++ webapp/www/search-searched.php
@@ -9,21 +9,18 @@ $searches = [];
 $searchterms = array(
 	$_POST['fieldname'] => $_POST['searchterm']
 );
-var_dump($searchterms);
 $searches = search_form_submissions($_POST['formid'], $searchterms);
 
 if($searches == false){
 	echo 'No Results Found';
 }
-if($searches == true){
+else{
 	foreach($searches as $search){
-	
 		$form = get_form_by_id($search['formid']);
-		$submissions[] = ['name' => $form['name'], 'id' => $search['id']];
+		$user = get_user_by_id($search['user']);
+		$submissions[] = ['name' => $form['name'], 'id' => $search['id'], 'user' => $user['name'], 'email' =>$user['email']];
 	
 	}
 }
-
-
-	echo $twig->render('search-searched.html',['formid' => $_POST['formid'],'searches' => $searches, 'name' =>$forms]);
+	echo $twig->render('search-searched.html',['formid' => $_POST['formid'],'searches' => $submissions, 'name' =>$forms]);
 ?>
